c8c94afceceaae4502e548519fe9f4ee28d757dd
Todd Kronenberg <toddkronenberg@gmail.com>
2015-12-10 23:30:53 -0600

    Got click on the extension button open quotes page

--- background.js
+++ background.js
@@ -1,8 +1,15 @@
 chrome.contextMenus.create({
   'title': 'Save as Quick Quote',
-  'contexts': ['selection'],
-  'onclick': function(info, tab) {
-    var now = new Date();
-    chrome.storage.sync.set({[now]: {date: now, url: info.pageUrl, text: info.selectionText}});
-  }
+  'id': '001',
+  'contexts': ['selection']
 });
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+  var now = new Date();
+  chrome.storage.sync.set({[now]: {date: now, url: info.pageUrl, text: info.selectionText}});
+});
+
+chrome.browserAction.onClicked.addListener(function() {
+  chrome.tabs.create({url: 'quotes.html'});
+});
+
