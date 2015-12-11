4ac6b590be8f1a7c206990692329d41db1357a42
Todd Kronenberg <toddkronenberg@gmail.com>
2015-12-10 21:21:20 -0600

    Save date, text, and url on context-menu click.

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ chrome.contextMenus.create({
   'title': 'Save as Quick Quote',
   'contexts': ['selection'],
   'onclick': function(info, tab) {
-    console.log('text', info.selectionText);
-    console.log('url', info.pageUrl);
+    var now = new Date();
+    chrome.storage.sync.set({[now]: {date: now, url: info.pageUrl, text: info.selectionText}});
   }
 });
